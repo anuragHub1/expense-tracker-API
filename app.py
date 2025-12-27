9a4d45f0c961894a10b6718bde688a557c25bcec
Anurag Kumar <anuragkrs2195@gmail.com>
2025-12-27 19:58:27 +0530

    setup sqlalchemy, user model and fix import issue

--- app.py
+++ app.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from src.db.session import engine
+from src.db.base import Base
+from src.db.models import user
 
 app=FastAPI(title="Expense tracker API")
+
+
+@app.on_event("startup")
+def on_startup():
+    Base.metadata.create_all(bind=engine)
 
 @app.get("/")
 def welcome():
